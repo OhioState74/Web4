a270b7eb7df7cf7ddeead14527f3c65e315dd537
eugene <superanubis@hotmail.com>
2023-03-01 21:13:35 +0200

    01.03

--- bboard/views.py
+++ bboard/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Bb
+
+
 # Create your views here.
 
 
 def index(request):
-    # return HttpResponse('index')
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published')
-    context = {'bbs': bbs}
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.all()
 
-    # return render(request, 'bboard/index.html', {'bbs': bbs})
+    # template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    # context = {'bbs': bbs}
+    # return HttpResponse(template.render(context, request))
+
+    return render(request, 'bboard/index.html', {'bbs': bbs})
 
 
 def about(request):
